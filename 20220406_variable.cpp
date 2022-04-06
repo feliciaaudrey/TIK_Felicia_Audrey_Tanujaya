2e9022091766499980db458f3e14ebe2d3d24bc1
feliciaaudrey <103086019+feliciaaudrey@users.noreply.github.com>
2022-04-06 11:08:46 +0800

    Create 20220406_variable.cpp

---
+++ 20220406_variable.cpp
@@ -0,0 +1,12 @@
+//header
+#include <iostream>
+using namespace std;
+int main() {//begin of function
+ float angka;
+ string nama;//deklarasi variabel
+ nama = "Felicia Audrey Tanujaya"; //masukkan value kedalam variabel
+ angka = 10.21+3.14;
+ cout<<nama<<endl;//menampilkan variabel
+ cout<<angka;
+ 
+}//end of function
